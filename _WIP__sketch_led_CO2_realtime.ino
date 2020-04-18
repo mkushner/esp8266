f0f600c170d1c2fb79d8737b7b0d7f80dae245d9
mkushner <maxim.kushner@gmail.com>
2020-04-18 16:20:59 +0300

    Update _WIP__sketch_led_CO2_realtime.ino
    added SDS011 (PM2.5/10) support
    added ThingSpeak integration
    local cache set up to 500 measures to prevent OutOfMemory
    local&cloud WS only run if WIFI connection established

--- _WIP__sketch_led_CO2_realtime.ino
+++ _WIP__sketch_led_CO2_realtime.ino
@@ -4,37 +4,50 @@
 #include <ESP8266WiFi.h>    // http://esp8266.github.io/Arduino/versions/2.0.0/doc/libraries.html#wifi-esp8266wifi-library
 #include <ESP8266WebServer.h>
 #include <ESP8266HTTPClient.h>
+#include <SDS011.h>
+#include "ThingSpeak.h"
 
-const bool DEBUG = false;   // true for Serial output
-const int INTERVAL = 30000; // ms between measurement
+const bool DEBUG = false;          // true for Serial output
+const int  INTERVAL = 60000;      // ms between measurements
+const bool localWS = true;        // run localCache ws  
+const bool cloudWS = true;        // run ThingSpeak integration
 
 // setup WIFI
-const char* ssid = "ssid";
-const char* pass = "pass";
+const char* ssid = "wifi ssid";
+const char* pass = "wifi pass";
+WiFiClient client;
+
+// setup ThingSpeak
+unsigned int channelID = "TS channel";           // ThingSpeak channel ID
+const char * APIkey = "TS api";  // ThingSpeak WriteAPI key
 
 // setup WS
 ESP8266WebServer server(80);
 int status = WL_IDLE_STATUS;
-String wsDataArray[1000];   // local WS cache 1000 measures
+String wsDataArray[500];    // local WS cache 500 measures
                                            
 // connect MH-Z19b via digitalPin 
-SoftwareSerial mySerial(13, 15);                                      //Rx->D8, Tx->D7 - actual pinout here https://chewett.co.uk/blog/1066/pin-numbering-for-wemos-d1-mini-esp8266/
+SoftwareSerial myMHZ(13, 15);                                         // Rx->D8, Tx->D7 - actual pinout here https://chewett.co.uk/blog/1066/pin-numbering-for-wemos-d1-mini-esp8266/
 byte cmd[9] = {0xFF,0x01,0x86,0x00,0x00,0x00,0x00,0x00,0x79};         // official datasheet w/byte commands: https://www.winsen-sensor.com/d/files/MH-Z19B.pdf
-//byte cmd_conf[9] = {0xFF,0x01,0x99,0x00,0x00,0x00,0x13,0x88,0xCB};  //5000ppm range - a list of useful byte cmds: https://revspace.nl/MHZ19
+//byte cmd_conf[9] = {0xFF,0x01,0x99,0x00,0x00,0x00,0x13,0x88,0xCB};  // 5000ppm range - a list of useful byte cmds: https://revspace.nl/MHZ19
 char result_raw[32];
 unsigned char response[9];
 int ppm=0;
 long millisMeasure=0;
 long millisCurrent=0;
 
-// connect LED (using 1.3' I2C SH1106)
+// connect SDS011 via digitalPin
+SDS011 mySDS;
+float p10, p25 = 0;
+
+// connect LED (using 1.3' I2C SH1106) via SDA/SCK
 SH1106Wire display(0x3c, 4, 14);     // start address, SDA->D2, SCL->D5
 
 
 void setup()
 {
 
-    // LED init in DEBUG mode
+    // built-in LED init in DEBUG mode
     if (DEBUG) {
     pinMode(LED_BUILTIN, OUTPUT);
     }
@@ -53,23 +66,51 @@ void setup()
     drawText("Wemos: 115200");
     delay(5000);
 
-    //CO2 init
-    mySerial.begin(9600);   // MH-Z19b works at 9600                           
+    // show config
+    drawText("Reading config");
+    delay(5000);
+    drawText("DEBUG mode: " + String(DEBUG));
+    delay(1000);
+    drawText("localWS support: " + String(localWS));
+    delay(1000);
+    drawText("cloudWS support: " + String(cloudWS));
+    delay(1000);
+
+    // CO2 init
+    myMHZ.begin(9600);   // MH-Z19b works at 9600                           
     if (DEBUG) {
       Serial.println("CO2 9600 init");
     }
     drawText("MH-Z19b: 9600");
     delay(5000);
 
-    //mySerial.write(cmd_conf, 9);  // uncomment and change cmd_conf for setting up measurement range (default 0-5000)
+    //myMHZ.write(cmd_conf, 9);  // uncomment and change cmd_conf for setting up measurement range (default 0-5000)
 
+    if (DEBUG) {
+      Serial.println("PPM 0-500");
+    }
     drawText("PPM: 0-5000");
     delay(5000);
 
-    drawText("Interval: " + String((INTERVAL/1000)) + "s");
+    if (DEBUG) {
+      Serial.println("Interval: " + String(INTERVAL/1000) + " s");
+    }
+    drawText("Interval: " + String(INTERVAL/1000) + "s");
     delay(5000);
 
-    //WIFI setup
+    // SDS011 init
+    mySDS.begin(5, 2); //D1, D4, Rx, Tx
+
+    if (DEBUG) {
+      Serial.println("PM sensor init");
+    }
+    drawText("PM sensor init");
+    delay(5000);
+
+    // WIFI setup
+    if (DEBUG) {
+      Serial.println("Connecting WIFI");
+    }
     drawText("Connecting WIFI");
     delay(5000);
     
@@ -78,7 +119,7 @@ void setup()
     }
 
     int wifi_try = 0;
-    while ((WiFi.status() != WL_CONNECTED) && wifi_try<5) {
+    while ( (WiFi.status() != WL_CONNECTED) && (wifi_try < 5) ) {
       if (DEBUG) {
         Serial.println("Couldn't connect on try " + String(wifi_try));
       }
@@ -98,23 +139,34 @@ void setup()
     }
     else {
       if (DEBUG) {
-        Serial.println(WiFi.status());
+        Serial.println("Connected to WIFI on " + String(wifi_try) + " try");
         Serial.println(WiFi.localIP());
         }
-    
         drawText(WiFi.localIP().toString());
         delay(5000);
     
         //WS setup
-        server.on ("/", WebServer);    
-        server.begin();
-    
-        if (DEBUG) {
-        Serial.println("WS ready");
+        if (localWS) {
+          server.on ("/", WebServer);    
+          server.begin();
+      
+          if (DEBUG) {
+          Serial.println("WS ready");
+          }
+          drawText("WS ready");
+          delay(5000);
         }
-    
-        drawText("WS ready");
-        delay(5000);
+
+        //TS setup
+        if (cloudWS) {
+          ThingSpeak.begin(client);
+          if (DEBUG) {
+          Serial.println("TS ready");
+          delay(5000);
+          }
+          drawText("TS ready");
+          delay(5000);
+        }
     }
 }
 
@@ -122,11 +174,25 @@ void drawText( String s) {
 
     display.clear();
     display.setTextAlignment(TEXT_ALIGN_LEFT);
-    display.setFont(ArialMT_Plain_16);
+    display.setFont(ArialMT_Plain_10);
     display.drawString(0, 10, s);
     display.display();
     
 }
+
+void getPM() {
+  
+    int error = mySDS.read(&p25, &p10);
+    if (!error) {
+      Serial.println("P2.5: " + String(p25));
+      Serial.println("P10:  " + String(p10));
+    }
+
+    else {
+      Serial.println("error reading PM");
+    }
+  
+  }
 
 void getCO2() {
 
@@ -136,20 +202,20 @@ void getCO2() {
         
     if (DEBUG) {
     Serial.println("Checking Serial2");
-    Serial.println(mySerial.available());
+    Serial.println(myMHZ.available());
     Serial.println("Sending Serial2 CMD");
     } 
     else {
-      mySerial.available(); // handle 1-byte offset 
+      myMHZ.available(); // handle 1-byte offset 
       }       
     
-    mySerial.write(cmd, 9);
+    myMHZ.write(cmd, 9);
 
     if (DEBUG) {
     Serial.println("Reading response:");
     }
     memset(response, 0, 9);
-    mySerial.readBytes(response, 9);
+    myMHZ.readBytes(response, 9);
 
     if (DEBUG) {
     sprintf(result_raw, "RAW: %02X %02X %02X %02X %02X %02X %02X %02X %02X", response[0], response[1], response[2], response[3], response[4], response[5], response[6], response[7], response[8]);
@@ -162,9 +228,8 @@ void getCO2() {
 
     if (DEBUG) {
     Serial.println("CO2 PPM:" + String(ppm));
-    } 
-    sendWSData(String(ppm));
-
+    }
+    
     millisMeasure = millis();
 
     if (DEBUG) {
@@ -182,7 +247,7 @@ void WebServer() {
   reply += "<html><body><hr><table border=1>";
   reply +="<tr><td>Current CO2:</td><td>" + String(wsDataArray[0]) + "</td></tr>";
    
-  for (int i = 0; i < 1000; i++) {
+  for (int i = 0; i < 500; i++) {
     reply += "<tr><td>" + String(i+1) + "</td><td>" + String(wsDataArray[i]) + "</td><tr>"; 
   }
   
@@ -193,27 +258,69 @@ void WebServer() {
 
 void sendWSData(String s) {
 
-  for (int i = 999; i > 0; i--) {
+  for (int i = 499; i > 0; i--) {
     wsDataArray[i] = wsDataArray[i - 1];
   }
   wsDataArray[0] = s;
   
 }
 
+void sendTSData( String s) {
+  
+  int httpCode = ThingSpeak.writeField(channelID, 1, s, APIkey);
+  
+  if (httpCode == 200) {
+    if (DEBUG) {
+      Serial.println("TS data successfully sent: status " + String(httpCode)); 
+    }
+    else {
+      Serial.println("error sending TS data: status " + String(httpCode)); 
+      }
+  }
+  
+}
+
 void loop()
 {   
-    if (WiFi.status() == WL_CONNECTED) {
+    if ( (WiFi.status() == WL_CONNECTED) && localWS ) {
     server.handleClient();
     }
     
-    drawText("CO2: " + String(ppm) + " ppm");
+    //drawText("CO2: " + String(ppm) + " ppm");
+    display.clear();
+    display.setTextAlignment(TEXT_ALIGN_LEFT);
+    display.setFont(ArialMT_Plain_10);
+    display.drawString(0, 10, "CO2: " + String(ppm) + " ppm");
+    display.drawString(0, 30, "P10: " + String(p10) + ", P2.5: " + String(p25));
+    display.display();
 
     if ((millis() - millisMeasure) > INTERVAL) // min delay recommended 10s not to overload MH-Z19b sensor
     {
-      getCO2();
       if (DEBUG) {
-      Serial.println("call getCO2()"); 
+        Serial.println("call getCO2()"); 
       }
+      getCO2();
+      
+      if (DEBUG) {
+        Serial.println("call getPM()"); 
+      }
+      getPM();
+
+
+      if (localWS) {
+        if (DEBUG) {
+          Serial.println("write to local WS");
+        }
+        sendWSData(String(ppm));
+      }
+
+      if (cloudWS) {
+        if (DEBUG) {
+          Serial.println("write to TS");
+        }
+        sendTSData(String(ppm));
+      }
+ 
     }
 
 }
